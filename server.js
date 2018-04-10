0b4b2ee053b2164995a8df94133693eb2f930007
Neil Gentleman <ngentleman@gmail.com>
2018-04-10 15:42:30 -0700

    styled

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({
+  extended: false,
+  limit: '10kb',
+}));
 
-const db = [];
+let db = '';
 
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Hey', message: 'Hello there!' });
-})
+  res.render('index', { pasta: db });
+});
+
+app.post('/update', (req, res) => {
+  db = req.body.pasta;
+  res.redirect('/#saved')
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}`))
+app.listen(port, () => console.log(`Listening on port ${port}`));
