e0b76ea4a760b7d5df95fce3fdc9dfa1e388bf25
Neil Gentleman <ngentleman@gmail.com>
2018-04-10 17:04:26 -0700

    compress responses

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 const express = require('express');
+const compression = require('compression');
 const bodyParser = require('body-parser');
 
 const app = express();
 app.set('view engine', 'pug');
+app.use(compression());
 app.use(bodyParser.urlencoded({
   extended: false,
   limit: '10kb',
 }));
 
-let db = '';
+let db = { pasta: '', date: new Date() };
 
 app.get('/', (req, res) => {
-  res.render('index', { pasta: db });
+  res.render('index', db);
 });
 
 app.post('/update', (req, res) => {
-  db = req.body.pasta;
+  db = {
+    pasta: req.body.pasta,
+    date: new Date(),
+  };
   res.redirect('/#saved');
 });
 
