ad1e07af9746580f8bdec7bf10a23607853be218
Neil Gentleman <ngentleman@gmail.com>
2022-06-02 15:39:25 -0700

    add CSP

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { text, send } = require('micro');
 const pug = require('pug');
 const qs = require('querystring');
 const url = require('url');
+const crypto = require('crypto');
 
 const isProd = process.env.NODE_ENV === 'production';
 
@@ -12,10 +13,13 @@ function view(name) {
 }
 
 module.exports = async (req, res) => {
+  const nonce = crypto.randomBytes(16).toString('base64');
+  res.setHeader('Content-Security-Policy', `object-src 'none'; script-src 'nonce-${nonce}' 'unsafe-inline'; base-uri 'none';`);
+
   const { pathname } = url.parse(req.url);
   if (pathname === '/') {
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
-    send(res, 200, view('index')(db));
+    send(res, 200, view('index')({ ...db, nonce }));
   } else if (req.method === 'POST' && pathname === '/update') {
     const body = qs.parse(await text(req, { limit: '10kb' })).pasta;
     db = { pasta: body, date: new Date() };
